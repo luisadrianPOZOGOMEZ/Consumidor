fe1228e9e878a56604e9f6ba6597da544e3eaf77
luisadrianPOZOGOMEZ <luispozoengineer@gmail.com>
2023-06-28 08:53:07 -0600

    first commit

--- consumer.js
+++ consumer.js
@@ -1,29 +1,51 @@
-const amqp = require('amqplib')
+import amqp from "amqplib";
+import fetch from "node-fetch";
 
 const rabbitSettings = {
-    protocol: 'amqp',
-    hostname: 'localhost',
-    port: 5672,
-    username: 'guest',
-    password: 'guest'
-}
+  protocol: "amqp",
+  hostname: "3.220.100.252",
+  port: 5672,
+  username: "pozodev",
+  password: "24LN112003",
+};
+
 async function connect() {
-    const queue = "estafeta"
-    try {
-        const conn = await amqp.connect(rabbitSettings);
-        console.log('Conexión exitosa')
-        const channel = await conn.createChannel()
-        console.log ("Canal creado exitosamente")
+  const queue = "event-initial";
+  try {
+    const conn = await amqp.connect(rabbitSettings);
+    console.log("Conexión exitosa");
+    const channel = await conn.createChannel();
+    console.log("Canal creado exitosamente");
 
-        channel.consume(queue, (msn)=> {
-            console.log(msn.content.toString())
-            channel.ack(msn)
-        })
-        
+    channel.consume(queue, async (msn) => {
+      const data = msn.content.toString();
+      console.log("Recibido: ", data);
+      const product = JSON.parse(data);
 
-    } catch (error) {
-        console.error('Error => ', error)    
-    }
+      // Envío del objeto user a la API para guardar usuarios
+      try {
+        const response = await fetch("http://44.216.17.250:3000/products", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(product),
+        });
+        if (response.ok) {
+          console.log("Usuario guardado correctamente");
+        } else {
+          console.error("Error al guardar usuario");
+        }
+      } catch (error) {
+        console.error("Error en el fetch: ", error);
+      }
+
+      channel.ack(msn);
+    });
+
+  } catch (error) {
+    console.error("Error => ", error);
+  }
 }
 
 connect();
